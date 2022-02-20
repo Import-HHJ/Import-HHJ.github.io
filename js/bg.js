d5afc53870f9eb8b3f2aaf9fc50a535c65129777
Import-HHJ <qkrtjdwns0604@gmail.com>
2022-02-21 01:46:03 +0900

    Momentum

--- js/bg.js
+++ js/bg.js
@@ -1,4 +1,4 @@
-const images = ["b.jpeg", "c.jpeg", "d.jpeg", "e.jpeg", "f.jpeg"];
+const images = ["b.jpeg", "c.jpeg", "d.jpeg", "e.jpeg", "f.jpeg","g.jpeg", "h.jpeg", "i.jpeg","j.jpeg","l.jpeg"];
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
 
