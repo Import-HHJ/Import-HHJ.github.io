36d3021ec79fda4250e3a251da0e71a931ecf8f6
Import-HHJ <qkrtjdwns0604@gmail.com>
2022-02-21 02:15:40 +0900

    Momentum

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,19 @@
-const images = ["b.jpeg", "c.jpeg", "d.jpeg", "e.jpeg", "f.jpeg","g.jpeg", "h.jpeg", "i.jpeg","j.jpeg","l.jpeg"];
+const images = [
+  "a.jpg",
+  "b.jpeg",
+  "c.jpeg",
+  "d.jpeg",
+  "e.jpeg",
+  "f.jpeg",
+  "g.jpg",
+  "h.jpg",
+  "i.jpg",
+  "j.jpg",
+  "k.jpg",
+  "l.jpg",
+];
 
 const choseImage = images[Math.floor(Math.random() * images.length)];
+console.log(choseImage);
 
 document.body.style.backgroundImage = `url('img/${choseImage}')`;
